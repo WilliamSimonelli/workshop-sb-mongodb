c0d76c25028895ebac7c317a3c76491f8e2b89b7
WilliamSimonelli <williamsimonelli@usp.br>
2021-03-10 17:18:20 -0300

    first commit

---
+++ src/test/java/com/will/workshopMDB/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.will.workshopMDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

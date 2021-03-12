9dc1c5a602db14d5c8c6ba7cac8f73596eef1a5d
WilliamSimonelli <williamsimonelli@usp.br>
2021-03-12 11:20:41 -0300

    DTO User

--- src/main/java/com/will/workshopMDB/resources/UserResources.java
+++ src/main/java/com/will/workshopMDB/resources/UserResources.java
@@ -1,6 +1,7 @@
 package com.will.workshopMDB.resources;
 
 
+import com.will.workshopMDB.DTO.UserDTO;
 import com.will.workshopMDB.domain.User;
 import com.will.workshopMDB.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 @RestController
@@ -22,9 +24,10 @@ public class UserResources {
     private UserService service;
 
     @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity<List<User>> findAll(){
+    public ResponseEntity<List<UserDTO>> findAll(){
         List<User> list = service.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDTO> listDTO = list.stream().map(x ->new UserDTO(x)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDTO);
     }
 
 
